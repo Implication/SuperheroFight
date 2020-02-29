a64ecae6e1f63a62bbd30f1a35fc0badb9c009be
Implication <IamTrajon@gmail.com>
2020-02-29 05:15:11 -0800

    Fixing port variable

--- server.js
+++ server.js
@@ -85,6 +85,6 @@ app.get("/fight", (req, res) => {
   console.log(firstHeroImage);
 });
 
-app.listen(3000, () => {
+app.listen(process.env.PORT || 3000, () => {
   console.log("server is listening on port 3000");
 });
